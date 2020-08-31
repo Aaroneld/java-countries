d9ec03fa5e437f3cb523535d3db9c6f3e1883b33
Aaroneld <Stateradio1!?>
2020-08-31 11:48:47 -0400

    Initialize Spring

---
+++ src/test/java/lambda/aaron/countries/CountriesApplicationTests.java
@@ -0,0 +1,13 @@
+package lambda.aaron.countries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
